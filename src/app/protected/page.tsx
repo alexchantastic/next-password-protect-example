3c33aefa9ddf2d0b629552f830a21a234fce5f41
Alex Chan <alexchantastic@gmail.com>
2024-02-10 21:44:28 -0800

    add callbackUrl

--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -6,7 +6,7 @@ export default async function Protected() {
   const session = await getServerSession(authOptions);
 
   if (!session) {
-    redirect('/api/auth/signin');
+    redirect('/api/auth/signin?callbackUrl=/protected');
   }
 
   return (
