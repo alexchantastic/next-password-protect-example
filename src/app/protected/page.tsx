0aa9d896f880bc7c2322b7278cdf66125867bab5
Alex Chan <alexchantastic@gmail.com>
2024-09-22 17:14:24 -0700

    protect server component route

--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -1,4 +1,13 @@
+import { getSession } from "@/utils/auth";
+import { redirect } from "next/navigation";
+
 export default async function Protected() {
+  const session = await getSession();
+
+  if (!session.isAuthenticated) {
+    redirect("/sign-in");
+  }
+
   return (
     <main>
       <h1>🔒 Protected page</h1>
