6b7fc82f39eafa86a5fdb85a1fb9b09738387b66
Alex Chan <alexchantastic@gmail.com>
2024-09-29 13:29:54 -0700

    add middleware protection

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,20 @@
-export function middleware() {
-  return;
+import { NextRequest, NextResponse } from "next/server";
+import { getSession } from "@/utils/auth";
+
+export async function middleware(request: NextRequest) {
+  const session = await getSession();
+  const redirectPath = new URL(request.url).pathname;
+
+  if (!session.isAuthenticated) {
+    return NextResponse.redirect(
+      new URL(
+        `/sign-in?redirect=${encodeURIComponent(redirectPath)}`,
+        request.url,
+      ),
+    );
+  }
+
+  return NextResponse.next();
 }
 
 export const config = { matcher: ["/middleware-protected/:path*"] };
